9759956ee733b72cf63de50d3f07db22a2844dae
gary2504 <garychaves25@gmail.com>
2024-08-07 15:29:51 -0600

    clases y funciones para el funcionamiento del creador del codigo o juego y el adivinador del juego

--- mastermind.py
+++ mastermind.py
@@ -4,14 +4,14 @@ class Mastermind:
     COLORES = {"R": fg("red"), "B": fg("blue"), "G": fg("green"), "Y": fg("yellow")}
     ColorCorrecto = fg("green") + "O" + attr("reset")
     ColorDesubicado = fg("yellow") + "O" + attr("reset")
-    def __init__(self):
-        self.tablero = [[' ']*4 + [' '] for _ in range(12)]
     def mostrar(self):
         for fila in self.tablero:
             adivinanza = " ".join([self.COLORES.get(color, '') + 'O' + attr('reset') for color in fila[:4]])
             indicaciones = " ".join(fila[4])
-            print(f"{adivinanza} {indicaciones}")
-            
+            print(f"{adivinanza} | {indicaciones}")
+       
+    def __init__(self):
+        self.tablero = [[' ']*4 + [' '] for _ in range(12)]     
     def actualizar(self, turno, adivinanza, indicaciones):
         self.tablero[turno][:4] = adivinanza
         self.tablero[turno][4] = indicaciones
@@ -22,21 +22,35 @@ class CreadorCodigo:
         self.codigo_secreto = self.crear_codigo_secreto()
     def crear_codigo_secreto(self):
         return [random.choice(self.colores) for _ in range(4)]
-    def obtener_retroalimentacion(self, adivinanza):
+    def ObtenerIndicaciones(self, adivinanza):
         indicaciones = []
-        codigo_secreto_copia = self.codigo_secreto[:]
+        CopiaCodigoColoresSecreto = self.codigo_secreto[:]
         for i in range(4):
-            if adivinanza[i] == codigo_secreto_copia[i]:
+            if adivinanza[i] == CopiaCodigoColoresSecreto[i]:
+                indicaciones.append(Mastermind.ColorCorrecto)
+                CopiaCodigoColoresSecreto[i] = None 
+        for i in range(4):
+            if adivinanza[i] in CopiaCodigoColoresSecreto:
                 indicaciones.append(Mastermind.ColorDesubicado)
-                
-        codigo_secreto_copia[codigo_secreto_copia.index(adivinanza[i])] = None
+                CopiaCodigoColoresSecreto[CopiaCodigoColoresSecreto.index(adivinanza[i])] = None
         return indicaciones
     
-    class AdivinarJuego:
-        def __init__(self, colores):
+class AdivinadorDeJuego:
+    def __init__(self, colores):
             self.colores = colores
-        def HacerAdivinanaza(self):
+            
+    def HacerAdivinanaza(self):
             adivinanza = input("Agregue su adivinanza de colores:").upper()
             while len(adivinanza) != 4 or any(c not in self.colores for c in adivinanza):
              adivinanza = input("Adivinaza incorrecta").upper()
             return list(adivinanza)
+
+
+
+
+
+
+
+
+
+
